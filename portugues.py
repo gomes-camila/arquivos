cc6e16d302152a16c0bcaa5f31f60ba38b9d5b72
Camila Gomes <camilagomesbsi@gmail.com>
2022-10-14 18:40:44 -0300

    criando funcoes

--- portugues.py
+++ portugues.py
@@ -1,36 +1,45 @@
-#Crie um programa que leia um livro no formato de texto em um idioma diferente do inglês.
-#Crie o gráfico de barras com a frequência das letras nesse idioma.
-#O gráfico deve conter Título, e os rótulos dos valores de cada barra.
-#Opcionalmente, inclua mais um ou 2 idiomas e faça gráficos de barras múltiplos, de forma a comparar as frequências de letras nos diversos idiomas.
-#O programa pode ser feito no formato de um script python (.py) ou no Google Colab.
-#No caso do programa feito no Google Colab, ele deve ser feito em um arquivo novo, estar completo numa única caixa de código e ser compartilhado com o professor.
-#Se for feito em um arquivo .py, deve ser colocado no github de forma pública e utilizar o poetry para a instalação dos pacotes necessários.
-
 from collections import Counter
 import matplotlib.pyplot as plt
 
 #PORTUGUÊS
 
+def main():
 #abrir arquivo localmente
-with open("helena.txt", encoding="cp437", errors="ignore") as file:
-  texto_pt = file.read().lower()
+  with open("helena.txt", encoding="cp437", errors="ignore") as file:
+    texto_pt = file.read().lower()
+  print("Livro: Helena \nAutor: Machado de Assis \nIdioma: Português")
 
-print("Livro: Helena \nAutor: Machado de Assis \nIdioma: Português")
+  grafico_letras_pt(conta_letras_pt(limpa_string_pt(texto_pt)))
+  lista_letras_pt
 
+def limpa_string_pt(par_texto_pt):
 #limpando string
-letras_pt = [c for c in texto_pt if c.isalpha()]
+  letras_pt = [c for c in par_texto_pt if c.isalpha()]
+  return letras_pt
 
+
+def conta_letras_pt(par_letras_pt):
 #criando abjeto counter
-frequencia_letras_pt = Counter(letras_pt)
+  frequencia_letras_pt = Counter(par_letras_pt)
+  print (f"{len(frequencia_letras_pt)} letras únicas identificadas")
+  return frequencia_letras_pt
 
-#quantidade de letras identificadas e lista de aparição das letras
-print (f"{len(frequencia_letras_pt)} letras únicas identificadas")
 
-for letra, qtde in frequencia_letras_pt.most_common():
-    print (f"{letra}: {qtde}")
+def lista_letras_pt(par_frequencia_letras_pt):
+#lista de aparição das letras
+  for letra, quantidade in par_frequencia_letras_pt.most_common():
+    print (f"{letra}: {quantidade}")
 
+
+def grafico_letras_pt(par_frequencia_letras_pt):
 #grafico das letras mais frequentes
-rotulos, valores = zip(*frequencia_letras_pt.most_common())
-plt.title("Frequência de letras em portugês")
-plt.bar(rotulos, valores)
-plt.show()
+  rotulos, valores = zip(*par_frequencia_letras_pt.most_common())
+  plt.title("Frequência de letras em portugês")
+  plt.ylabel("Frequência")
+  plt.xlabel("Letras")
+  plt.legend
+  plt.bar(rotulos, valores)
+  plt.show()
+
+
+main()
